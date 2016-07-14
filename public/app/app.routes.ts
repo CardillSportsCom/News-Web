26ad8e7ebbfe6ca2a7503312016adfe36f012176
Vithushan Nama <vithushan@Vithushans-MacBook-Pro.local>
2016-07-13 23:10:03 -0400

    delete old route files

--- public/app/app.routes.ts
+++ public/app/app.routes.ts
@@ -1,18 +1,11 @@
 import {RouterConfig, provideRouter} from '@angular/router';
 import { Location, LocationStrategy, HashLocationStrategy } from '@angular/common';
 
-import { CustomersRoutes } from './articlesPage/articlesPage.routes';
-import { CustomerRoutes } from './+articlePage/customer.routes';
-import { HomeRoutes } from './homePage/home.routes';
-
 import { HomeComponent } from './homePage/home.component';
 import { ArticlesPageComponent } from './articlesPage/articlesPage.component';
 import { ArticleDetailsComponent } from './+articlePage/articleDetails.component'
 
 export const App_Routes: RouterConfig = [
-  //...CustomersRoutes,
-  //...CustomerRoutes,
-  //...HomeRoutes,
   { path: '', component: HomeComponent },
   { path: 'articles', component: ArticlesPageComponent } ,
   { path: 'articles/:id', component: ArticleDetailsComponent}
--- public/app/articlesPage/customers.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var customers_component_1 = require('./customers.component');
-exports.CustomersRoutes = [
-    {
-        path: 'customers',
-        component: customers_component_1.CustomersComponent
-    }
-];
-//# sourceMappingURL=customers.routes.js.map
